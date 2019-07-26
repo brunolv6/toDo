dd1a0630448ea88cfa0123acf50a40e638ffdb06
Bruno Valle <brunolv6@gmail.com>
2019-07-26 20:40:54 -0300

    First Commmit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      inputValue: "",
+      todo: []
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({inputValue: e.target.value});
+  }
+
+  handleAdd = () => {
+    let todos = this.state.todo;
+    todos.push(this.state.inputValue);
+    this.setState({todo: todos});
+    this.setState({inputValue: ""});
+  }
+
+  render(){
+    return(
+      <div className="App App-header"> 
+        <div>
+          <h1>Lista To Do</h1>
+          <input autoFocus type="text" placeholder="o que fará?"
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleAdd}>add</button>
+        </div>
+        {
+          this.state.todo.map(td => (
+            <h3> {td} </h3>
+          ))
+        }  
+      </div>
+    );
+  }
+
 }
 
 export default App;
